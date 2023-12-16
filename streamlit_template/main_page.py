8d3cb2a4d32340fd119ce1d01212f820d9dd5af7
Tyler Barty <tyler.barty+github@icloud.com>
2023-12-16 15:59:25 -0700

    updating to right link

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = '<a href="https://tylerbarty-mariokart-stats.streamlit.app" target="_self" a>To my Github Pages Site</a>'
+link = '<a href="https://tylerbarty.github.io/github_assignment/" target="_self" a>To my Github Pages Site</a>'
 st.markdown(link, unsafe_allow_html=True)
